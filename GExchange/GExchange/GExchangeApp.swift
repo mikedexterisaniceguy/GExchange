d8a9ac1184dd7ccb19bc86d9e6ec409f36ed9c47
mikedexterisaniceguy <mikedexterisaniceguy@gmail.com>
2023-07-26 13:55:36 +0300

    Initial commit
    Создал проект

---
+++ GExchange/GExchange/GExchangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GExchangeApp.swift
+//  GExchange
+//
+//  Created by 123 on 26.07.23.
+//
+
+import SwiftUI
+
+@main
+struct GExchangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
